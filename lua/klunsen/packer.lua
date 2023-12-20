e20df9d7b429611435ed1d2b26e77a773fa51a93
tichenor <klunsen@DESKTOP-77DQU2P>
2023-12-20 23:33:21 +0100

    lualine added w/ default settings

--- lua/klunsen/packer.lua
+++ lua/klunsen/packer.lua
@@ -58,4 +58,10 @@ return require('packer').startup(function(use)
 			{'rafamadriz/friendly-snippets'},
 		}
 	}
+
+	-- Lualine
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
 end)
